b8fae14cc5dc59a471aaf85cc8e16953cf4e7671
wniegoda <wiktoriach99@wp.pl>
2024-11-13 12:11:23 +0100

    koniec zajęć 13.11.24
    Byliśmy grzeczni pozdrawiam

--- R/src/skrypt_01.R
+++ R/src/skrypt_01.R
@@ -1,2 +1,20 @@
 # Załadowanie paczek
 library(tidyverse)
+
+# Dane nowa zelandia (z www)
+dane_www <- readr::read_csv(file = "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# wczytanie z dysku - pełna ścieżka
+dane_dysk_full_path <- readr::read_csv(file = "C:/GITHUB/projekt_końcowy/R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# wczytanie z dysku - sciezka wzgledna
+dane_dysk_relative_path <- readr::read_csv(file = "R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+
+# zmiana klumny tekstowej na liczbowa
+dane_dysk_relative_path$Value <- as.numeric(dane_dysk_relative_path$Value)
+
+#parsowanie tekstu na numery
+dane<- readr::read_csv(file = "R/data/annual-enterprise-survey-2023-financial-year-provisional.csv")
+dane$Value <- readr::parse_number(dane$Value)
+
+dane[748, "Value"]
